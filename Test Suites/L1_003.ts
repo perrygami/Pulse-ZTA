010b8cdfc4554584c7f2bd0699df23de14d65d26
Maitri.B <maitri.b@simformsolutions.com>
2020-08-27 19:12:13 +0530

    Radar charts, Test Suit

---
+++ Test Suites/L1_003.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>L1_003</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ca2b0f21-f678-4540-831d-21f6533c3c42</testSuiteGuid>
+   <testCaseLink>
+      <guid>a95cbef9-8599-4613-9bdc-f2e859594343</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/L1 Dashboard/Summary Panel</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0db37a37-a87b-49a5-83cd-01295a5915d7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/L1 Dashboard/Active anomalies</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b25ef593-386f-40f6-a65e-fce05f57c113</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/L1 Dashboard/TopLocations</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5b405885-afcf-43e0-b06b-d0799b278157</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/L1 Dashboard/TopActiveApplications</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c8160541-6cc1-4931-bb49-2d1b051ce69e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/L1 Dashboard/Non-complaince</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>132d941d-7d37-4de0-8a3d-ad5bce6362a8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/L1 Dashboard/TopGateways</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
